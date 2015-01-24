e20121fae1851a3ce0beb294ae854578d1cc7a21
Alistair Roche <roche.a@gmail.com>
2015-01-24 14:33:28 +1100

    Move TODOs to separate file

--- src/clj_sockets/core.clj
+++ src/clj_sockets/core.clj
@@ -74,10 +74,3 @@
   {:post connected?}
   (.accept server-socket))
 
-
-
-; TODO: file bug about checker defn blowing up when there are no type-hints in an interop method call
-; TODO: fully-qualified methods in non-nil-return highlight badly
-; TODO: the following causes assert failure:
-; (defn connected? [^Socket socket :- Socket] :- Boolean
-;(.isConnected Socket))
