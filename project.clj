0c0e06cd18eaeb29b0112ab5473d975d376651f3
Alistair Roche <roche.a@gmail.com>
2015-01-26 10:56:07 +1100

    Add codox plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-marginalia "0.8.0"]]
+  :plugins [[lein-marginalia "0.8.0"]
+            [codox "0.8.10"]]
+  :codox {:src-dir-uri "http://github.com/atroche/clj-sockets/blob/master/"
+          :src-linenum-anchor-prefix "L"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.2.77"]])
