cdf52bbace08e657c4f13f3a1bf9ef8458be2835
Alistair Roche <roche.a@gmail.com>
2015-01-26 10:52:03 +1100

    Use explicit core.typed/ann instead of defn macros
    So codox works, and because it looks a bit neater.

--- src/clj_sockets/core.clj
+++ src/clj_sockets/core.clj
@@ -7,9 +7,9 @@
 
 (ns clj-sockets.core
   "Contains all of the functions that make up the public API of this project."
-  (:require [clojure.core.typed :as t :refer [ann defn fn]]
+  (:require [clojure.core.typed :as t :refer [ann]]
             [clojure.java.io :refer [writer reader]])
-  (:refer-clojure :exclude [defn fn read-line])
+  (:refer-clojure :exclude [read-line])
   (:import (java.net Socket ServerSocket)
            (java.io BufferedWriter BufferedReader)
            (clojure.lang Seqable)))
@@ -21,27 +21,27 @@
 (ann ^:no-check clojure.java.io/reader [Socket -> BufferedReader])
 (ann ^:no-check clojure.core/line-seq [BufferedReader -> (Seqable String)])
 
+(ann create-socket [String Integer -> Socket])
 (defn create-socket
   "Connect a socket to a remote host. The call blocks until
    the socket is connected."
-  [^String hostname :- String ^Integer port :- Integer] :- Socket
+  [^String hostname ^Integer port]
   (Socket. hostname port))
 
+(ann close-socket [Socket -> nil])
 (defn close-socket
   "Close the socket, and also closes its input and output streams."
-  [^Socket socket :- Socket] :- nil
+  [^Socket socket]
   (.close socket))
 
-(defn write-to-buffer
+(ann write-to-buffer [BufferedWriter String -> nil])
+(defn ^:private write-to-buffer
   "Write a string to a BufferedWriter, then flush it."
-  ^:private [^BufferedWriter output-stream :- BufferedWriter
-             ^String string :- String] :- nil
+  [^BufferedWriter output-stream ^String string]
   (.write output-stream string)
   (.flush output-stream))
 
 
-;; These fns are annotated manually (instead of using defn) because
-;; core.typed blows up if I try to put it in the definition.
 (ann write-to [Socket String -> nil])
 (defn write-to
   "Send a string over the socket."
@@ -65,15 +65,19 @@
   If we didn't do this, every time we did e.g. read-char on a socket we'd
   get back a new reader, and that last one would be thrown away despite having
   loaded input into its buffer."
-  (memoize (fn [^Socket socket :- Socket] :- BufferedReader
-             (reader socket))))
+  (memoize (t/ann-form
+             (fn [^Socket socket]
+              (reader socket))
+             (t/IFn [Socket -> BufferedReader]))))
 
 (ann read-char [Socket -> Character])
 (defn read-char
   "Read a single character from a socket."
   [socket]
-  (let [read-from-buffer (fn [^BufferedReader input-stream :- BufferedReader] :- Integer
-                           (.read input-stream))]
+  (let [read-from-buffer (t/ann-form
+                           (fn [^BufferedReader input-stream]
+                            (.read input-stream))
+                           (t/IFn [BufferedReader -> Integer]))]
     (-> socket
         get-reader
         read-from-buffer
@@ -93,22 +97,26 @@
 (defn read-line
   "Read a line from the given socket"
   [^Socket socket]
-  (let [read-line-from-reader (fn [^BufferedReader reader :- BufferedReader] :- String
-                                (.readLine reader))]
+  (let [read-line-from-reader (t/ann-form
+                                (fn [^BufferedReader reader]
+                                  (.readLine reader))
+                                (t/IFn [BufferedReader -> String]))]
     (read-line-from-reader (get-reader socket))))
 
+(ann create-server [Integer -> ServerSocket])
 (defn create-server
   "Initialise a ServerSocket on localhost using a port.
 
   Passing in 0 for the port will automatically assign a port based on what's
   available."
-  [^Integer port :- Integer] :- ServerSocket
+  [^Integer port]
   (ServerSocket. port))
 
 (t/non-nil-return java.net.ServerSocket/accept :all)
+(ann listen [ServerSocket -> Socket])
 (defn listen
   "Waits for a connection from another socket to come through, then
    returns the server's now-connected Socket."
-  [^ServerSocket server-socket :- ServerSocket] :- Socket
+  [^ServerSocket server-socket]
   (.accept server-socket))
 
